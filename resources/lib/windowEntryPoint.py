648beb9ed306d7f8eacdd70a4b62fb2ccb3a0629
sitronet <rondrach@gmail.com>
2020-04-15 18:28:53 +0200

    big change reverse

--- resources/lib/windowEntryPoint.py
+++ resources/lib/windowEntryPoint.py
@@ -114,7 +114,7 @@ ACTION_VOLUME_DOWN = 89
 ACTION_VOLUME_UP = 88
 
 
-class OriginalSkin(pyxbmct.Skin):
+class MyOriginalSkin(pyxbmct.Skin):
     '''
     original source is :
     @property
@@ -125,10 +125,10 @@ class OriginalSkin(pyxbmct.Skin):
     def background_img(self):
         return os.path.join(self.images, 'AddonWindow', 'ContentPanel.png')
     '''
-
+    '''
     @property
     def background_img(self):
-        return xbmc.translatePath(os.path.join(ADDON.getAddonInfo('path'), 'resources', 'skins', 'Default', 'media', 'fond-noir.jpg'))
+        return xbmc.translatePath(os.path.join(ADDON.getAddonInfo('path'), 'resources', 'skins', 'Default', 'media', 'pcp_allegro.png'))
 
     @property
     def title_background_img(self):
@@ -137,27 +137,21 @@ class OriginalSkin(pyxbmct.Skin):
     @property
     def main_bg_img(self):
         return xbmc.translatePath(os.path.join(ADDON.getAddonInfo('path'), 'resources', 'skins', 'Default', 'media','pcp_vibrato.png'))
-
-
-pyxbmct.addonwindow.skin = OriginalSkin()
+    
+    
+    pyxbmct.addonwindow.skin = MyOriginalSkin()
+    '''
 
 # Then create your UI window class with the new background
 #class MyCoolWindow(pyxbmct.AddonWindow):
 
-def singleton(cls):
-    instance = [None]
-    def wrapper(*args, **kwargs):
-        if instance[0] is None:
-            instance[0] = cls(*args, **kwargs)
-        return instance[0]
-
-    return wrapper
-
 #class FenetreOne(pyxbmct.AddonFullWindow):    # background img (skin) header+ title could be hide ok
 #class FenetreOne(pyxbmct.AddonDialogWindow):  # background (skin) header+ title + on the top screen
 #class FenetreOne(pyxbmct.BlankDialogWindow):  # transparent without header+title and on top screen
-#class FenetreOne(pyxbmct.BlankFullWindow):    # black background without header+title (cannot change background ?) could be hide
-#class FenetreOne(pyxbmctExtended.AddonFullWindowWT)  # background (skin) without header+title - could be hide ?
+#class FenetreOne(pyxbmct.BlankFullWindow):    # black background without header+title (cannot change background) could be hide
+#class FenetreOne(pyxbmctExtended.BackgroundDialogWindow)  # background (skin) without header+title - could be hide
+#class FenetreOne(pyxbmctExtended.BackgroundFullWindow)  # background (skin) without header+title - could be hide
+
 #class
 class FenetreOne(pyxbmct.AddonFullWindow):
     '''
@@ -168,7 +162,7 @@ class FenetreOne(pyxbmct.AddonFullWindow):
     def __init__(self,*args, **kwargs):
         title = args[0]
         super(FenetreOne, self).__init__(title)
-        #super(FenetreOne, self).__init__()
+        #super(FenetreOne, self).__init__() # if class changed without title
         xbmc.log('Starting FenetreOne', xbmc.LOGNOTICE)
         self.promptStartScript()
         self.geometrie()
@@ -258,7 +252,6 @@ class FenetreOne(pyxbmct.AddonFullWindow):
         SIZESCREEN_HEIGHT = xbmcgui.getScreenHeight()            # exemple  # 1080
         SIZESCREEN_WIDTH = xbmcgui.getScreenWidth()                         # 1920
 
-        # I didn't know the operator '//' when I wrote this code
         self.GRIDSCREEN_Y = SIZESCREEN_HEIGHT // 10            # 108
         self.GRIDSCREEN_X = SIZESCREEN_WIDTH //10             # 192
 
@@ -271,21 +264,20 @@ class FenetreOne(pyxbmct.AddonFullWindow):
             self.screeny = SIZE_HEIGHT_pyxbmct
         #pyxbmct :
         self.setGeometry(width_=self.screenx, height_=self.screeny, rows_=NEUF, columns_=SEIZE)
-        xbmc.log('Size of grid FenetreOne set to : ' + str(self.screenx) + ' x ' + str(self.screeny), xbmc.LOGNOTICE)
+        xbmc.log('Size of FenetreOne set to : ' + str(self.screenx) + ' x ' + str(self.screeny), xbmc.LOGNOTICE)
 
-        self.image_dir = ARTWORK    # path to pictures used in the program (future development)
+        self.image_dir = ARTWORK    # path to pictures used in the program
 
         self.image_button_pause = self.image_dir + '/pause.png'   # get from Xsqueeze
         self.image_button_stop = self.image_dir + '/stop.png'     # get from Xsqueeze
         self.image_button_play = self.image_dir + '/play.png'     # get from Xsqueeze
-        self.textureback_slider_duration = self.image_dir + '/slider_back.png'  # get from plugin audio spotify
+        self.textureback_slider_duration = self.image_dir + '/slider_back.png'  # Get from plugin audio spotify
         self.texture_slider_duration = self.image_dir + '/slider_button_new.png'
-        self.image_list_focus = self.image_dir + '/MenuItemFO.png'        # get from myself
-        self.textureback_slider_volume = self.image_dir + '/slider_back.png'
-        self.texture_slider_volume = self.image_dir + '/slider_button_new.png'
+        self.image_list_focus = self.image_dir + '/MenuItemFO.png'        # Drawing myself from an original pyXBMCt
+        self.textureback_slider_volume = self.image_dir + '/slider_back.png'   # Get from jivelite (Squeezebox controler)
+        self.texture_slider_volume = self.image_dir + '/slider_button_new.png'  # Get from jivelite
 
-        self.imagedefond = self.image_dir + '/fond-noir.jpg'
-        #self.setBackground(self.imagedefond)
+        self.imagedefond = self.image_dir + '/fond-noir.jpg'    # Get somewhere Online
 
     def defineControlMenus(self):
         # list for the items of the menu
@@ -300,6 +292,7 @@ class FenetreOne(pyxbmct.AddonFullWindow):
         self.list_racine_label = pyxbmct.Label('Waiting Action...', textColor='0xFF808080')
         self.Information_textbox = pyxbmct.TextBox()
         self.listMenu_Racine = pyxbmct.List(buttonFocusTexture=self.image_list_focus, _itemHeight=40)
+        # To get marks on the screen :
         self.label0 = pyxbmct.Label('ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789_abcdefghijklmnopqrstuvwxyz')
         self.label1 = pyxbmct.Label('A')
         self.label2 = pyxbmct.Label('B')
@@ -315,12 +308,12 @@ class FenetreOne(pyxbmct.AddonFullWindow):
         self.labelU = pyxbmct.Label('U')
 
     def putControlElements(self):
-        ''' mainly place the menu list and other elements on the screen 
+        ''' mainly place the menu list and other controls elements on the screen
 
         '''
         row_depart = 3
         col_depart = row_depart
-        self.espace_row = NEUF - 5  # Pixel or tiles ?
+        self.espace_row = NEUF - 5  # tiles
         self.espace_col = (SEIZE // 2) - col_depart
 
         ligneLabel = NEUF - 2
@@ -329,7 +322,8 @@ class FenetreOne(pyxbmct.AddonFullWindow):
                           row= ligneLabel, 
                           column= 2, 
                           rowspan= 1, 
-                          columnspan= 10)
+                          columnspan= 10
+                          )
 
         self.placeControl(control=self.Information_textbox,
                           row= row_depart ,
@@ -339,7 +333,10 @@ class FenetreOne(pyxbmct.AddonFullWindow):
                           pad_x= 4,
                           pad_y= 4
                           )
-        self.Information_textbox.setText('Try to press : \n' 'OK, Select, ContextMenu or Volume+ or Volume- keys from your remote to watch in action')
+        self.Information_textbox.setText('Try to press : \n' 
+                                         'OK, Select, ContextMenu or Volume+ or Volume- keys \n' 
+                                         ' from your remote to watch in action'
+                                         )
         self.Information_textbox.setVisible(True)
 
         self.placeControl(control= self.listMenu_Racine,
@@ -350,7 +347,7 @@ class FenetreOne(pyxbmct.AddonFullWindow):
         
         # Add items to the list
         self.listMenu_Racine.addItems(self.listeRacinePourMenu)
-
+        # Add marks
         self.placeControl(control=self.label0, row=ligneLabel - 4, column=0, rowspan=1, columnspan= SEIZE // 2 )
         self.placeControl(control=self.label1, row=ligneLabel - 2, column=0, rowspan=1, columnspan=2)
         self.placeControl(control=self.label2, row=ligneLabel - 2, column=1, rowspan=1, columnspan=2)
@@ -383,13 +380,13 @@ class FenetreOne(pyxbmct.AddonFullWindow):
 
         if itemSelectionRacine == 'Fenetre 1 : AddonFullWindow':
 
-            self.fenetre1 = Fenetre1()
+            self.fenetre1 = Fenetre1('Can write a title -> Window n° 1')
             self.fenetre1.doModal()
             del self.fenetre1
 
         if itemSelectionRacine == 'Fenetre 2 : AddonDialogWindow':
 
-            self.fenetre2 = Fenetre2()
+            self.fenetre2 = Fenetre2('Can write a title -> Window n° 2')
             self.fenetre2.doModal()
             del self.fenetre2
 
@@ -412,7 +409,6 @@ class FenetreOne(pyxbmct.AddonFullWindow):
             self.fenetre6 = Fenetre6()
             self.fenetre6.doModal()
             del self.fenetre6
-            
 
     def promptVolume(self):
         volumeFrame =  VolumeFrameChild()
@@ -420,7 +416,7 @@ class FenetreOne(pyxbmct.AddonFullWindow):
         del volumeFrame
 
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in main Window ')
         contextMenuFrame.doModal()
         del contextMenuFrame
 
@@ -430,9 +426,9 @@ class Fenetre1(pyxbmct.AddonFullWindow):
     AddonFullWindow have a background
     '''
     def __init__(self, *args ):
-        #title = args[0] # for AddonFullWindow
+        title = args[0] # for AddonFullWindow
 
-        super(Fenetre1, self).__init__('Title -> Fenetre 1 ')
+        super(Fenetre1, self).__init__(title)
 
         '''set the geometry of the screen (main Window of the script, point of entry )
                 to place later elements and controls (list button image etc...)'''
@@ -452,16 +448,23 @@ class Fenetre1(pyxbmct.AddonFullWindow):
 
         self.image_dir = ARTWORK    # path to pictures used in the program
         # print a picture
-        self.cover_jpg = self.image_dir + '/music.png'      # pour le démarrage then updated
+        self.cover_jpg = self.image_dir + '/music.png'
         self.pochette = pyxbmct.Image(self.cover_jpg)
-        self.placeControl(self.pochette, 3 , int(SEIZE / 2) , 28 , 28 )  # to fix
+        self.placeControl(self.pochette, 3 , int(SEIZE / 2) , 28 , 28 ) #TODO: to adjust because is not always a square
         self.pochette.setImage(self.cover_jpg)
 
-        # print a fadeLabel
+        # place a fadeLabel on the grid: line(row) 8, column 4
+        # which spans 3 rows and 27 columns
         self.labeltitre = pyxbmct.FadeLabel()
-        self.placeControl(self.labeltitre,  8 , 4 , 3 , 27 )
+        self.placeControl(control=self.labeltitre,
+                          row= 8,
+                          column= 4,
+                          rowspan= 3,
+                          columnspan= 27
+                          )
         self.labeltitre.addLabel('Fenetre 1 : AddonFullWindow ')
 
+        # print a label to write the size of this window (self)
         self.labelSize = pyxbmct.Label('')
         self.placeControl(self.labelSize, 10 , 4 , 1 , 12)
         self.labelSize.setLabel(str(self.screenx) + ' X ' + str(self.screeny) )
@@ -511,7 +514,7 @@ class Fenetre1(pyxbmct.AddonFullWindow):
         del volumeFrame
 
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in Window 1')
         contextMenuFrame.doModal()
         del contextMenuFrame
 
@@ -523,8 +526,8 @@ class Fenetre2(pyxbmct.AddonDialogWindow):
     '''
 
     def __init__(self, *args ):
-        #title = args[0] # for AddonFullWindow and AddonDialogWindow
-        super(Fenetre2, self).__init__('title -> Fenetre 2 ')
+        title = args[0] # for AddonFullWindow and AddonDialogWindow
+        super(Fenetre2, self).__init__(title)
 
         '''set the geometry of the screen (main Window of the script, point of entry )
                 to place later elements and controls (list button image etc...)'''
@@ -603,19 +606,19 @@ class Fenetre2(pyxbmct.AddonDialogWindow):
         del volumeFrame
 
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in Window 2 ')
         contextMenuFrame.doModal()
         del contextMenuFrame
 
 
 class Fenetre3(pyxbmct.BlankDialogWindow):
     '''BlankFullWindow and BlankDialogWindow have not an header
-       BlankDialogWindow have a transparent background so we see just the element put by
+       BlankDialogWindow have a transparent background so we see just the elements put by
        placeControl()
        BlankFullWindow have a black background that hide the parent window'''
 
     def __init__(self, *args ):
-
+        #note : there isn't title here
         super(Fenetre3, self).__init__()
 
         '''set the geometry of the screen (main Window of the script, point of entry )
@@ -696,7 +699,7 @@ class Fenetre3(pyxbmct.BlankDialogWindow):
         del volumeFrame
 
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in Window 3')
         contextMenuFrame.doModal()
         del contextMenuFrame
 
@@ -789,17 +792,17 @@ class Fenetre4(pyxbmct.BlankFullWindow):
         del volumeFrame
 
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in Window 4')
         contextMenuFrame.doModal()
         del contextMenuFrame
 
 class Fenetre5(pyxbmctExtended.BackgroundDialogWindow):
     '''BackgroundFullWindow and BackgroundDialogWindow have not an header
         like BlankFullWindow and BlankDialogWindow
-        BackgroundDialogWindow have a transparent background beyond the window
+        BackgroundDialogWindow have a transparent background beyond the self.window
         so we see the parent Window but inside itself it have a background fix by skin (main_bg_img)
-        unlike BlankDialogWindow
-        BackgroundDialogWindow, it is an extension of the original pyxbmct librairy
+        unlike BlankDialogWindow (reminder all is transparent)
+        BackgroundDialogWindow, it is an extended class of the pyxbmct framework
     '''
 
     def __init__(self, *args ):
@@ -884,7 +887,7 @@ class Fenetre5(pyxbmctExtended.BackgroundDialogWindow):
         del volumeFrame
 
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in Window 5')
         contextMenuFrame.doModal()
         del contextMenuFrame
 
@@ -893,7 +896,7 @@ class Fenetre6(pyxbmctExtended.BackgroundFullWindow):
     '''BackgroundFullWindow and BackgroundDialogWindow doesn't have an header
         like BlankFullWindow and BlankDialogWindow.
         BackgroundFullWindow have a black background beyond the window
-        so we could not see the parent Window and inside itself it have a background fix by skin  (main_bg_img)
+        so we could not see the parent Window and inside itself it have a different background fix by skin  (main_bg_img)
         unlike BlankFullWindow
         This BackgroundFullWindow, it is an extension of the original pyxbmct
     '''
@@ -979,7 +982,7 @@ class Fenetre6(pyxbmctExtended.BackgroundFullWindow):
         del volumeFrame
 
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in Window 6')
         contextMenuFrame.doModal()
         del contextMenuFrame
 
@@ -992,8 +995,9 @@ class ContextMenuFrameChild(pyxbmct.AddonDialogWindow):
        Beyond the window, the parent window is visible
     '''
 
-    def __init__(self):
-        super(ContextMenuFrameChild, self).__init__('ContextMenu Key pressed')
+    def __init__(self, *args):
+        title = args[0]
+        super(ContextMenuFrameChild, self).__init__(title)
         # get image back
         self.image_dir = ARTWORK  # path to pictures used in the program
         self.cover_jpg = self.image_dir + '/music.png'
@@ -1029,7 +1033,7 @@ class ContextMenuFrameChild(pyxbmct.AddonDialogWindow):
 
         self.label_context_basdroite = pyxbmct.Label('BD')
         self.placeControl(control=self.label_context_basdroite, row=10, column=0, rowspan=1, columnspan=1)
-        self.label_context_basgauche = pyxbmct.Label('BG. 500 x 400 px')
+        self.label_context_basgauche = pyxbmct.Label('BG. 500 x 400 px') # it will print outside the window
         self.placeControl(control=self.label_context_basgauche, row=10, column=9, rowspan=1, columnspan=5)
 
         self.setFocus(self.button_context)
@@ -1051,8 +1055,8 @@ class VolumeFrameChild(pyxbmct.BlankDialogWindow):
         super(VolumeFrameChild, self).__init__()
         # get image back
         self.image_dir = ARTWORK  # path to pictures used in the program
-        self.textureback_slider_volume = self.image_dir + '/rem_sliderbar_bkgrd.png'
-        self.texture_slider_volume = self.image_dir + '/rem_slider.png'
+        self.textureback_slider_volume = self.image_dir + '/rem_sliderbar_bkgrd.png' #get form jivelite
+        self.texture_slider_volume = self.image_dir + '/rem_slider.png'  # get from jivelite
 
         # take care of pos_x & pos_y
         Size_W_ChildSelf = 400
@@ -1063,7 +1067,7 @@ class VolumeFrameChild(pyxbmct.BlankDialogWindow):
         # reminder :
         # we fit the parent window to 64 columns  x 32 rows with virtual pixel of 1280 x 720
         # the pos_x and pos_y are on the virtual pixel grid pyxbmct (1280 x 720)
-        # inside this child frame we define a grid : 5 rows X 10 columns
+        # inside this child frame we define a grid : 2 rows X 10 columns
         self.setGeometry(width_= Size_W_ChildSelf,
                          height_= Size_H_ChildSelf,
                          rows_= 2,
@@ -1093,12 +1097,21 @@ class VolumeFrameChild(pyxbmct.BlankDialogWindow):
     def set_info_controls(self):
 
         self.label_volume = pyxbmct.Label('volume')
-        self.placeControl(control=self.label_volume, row=0, column=0, rowspan=1, columnspan=10)
+        self.placeControl(control=self.label_volume,
+                          row= 0,
+                          column= 0,
+                          rowspan= 1,
+                          columnspan= 10)
 
         self.slider_volume = pyxbmct.Slider(textureback=self.textureback_slider_volume,
                                             texture=self.texture_slider_volume,
                                             texturefocus=self.texture_slider_volume, orientation=xbmcgui.HORIZONTAL)
-        self.placeControl(self.slider_volume, 1, 0, 1, 10)
+        self.placeControl(control= self.slider_volume,
+                          row= 1,
+                          column= 0,
+                          rowspan= 1,
+                          columnspan= 10
+                          )
         volumePercent = float(50)
         self.slider_volume.setPercent(volumePercent)
 
@@ -1168,8 +1181,7 @@ class VolumeFrameChild(pyxbmct.BlankDialogWindow):
             xbmc.log('else condition onAction ' + repr(action)  , xbmc.LOGNOTICE)
             self._executeConnected(action, self.actions_connected)
 
-
     def promptContextMenu(self):
-        contextMenuFrame = ContextMenuFrameChild()
+        contextMenuFrame = ContextMenuFrameChild('ContextMenu Key pressed in Volume Window')
         contextMenuFrame.doModal()
         del contextMenuFrame
